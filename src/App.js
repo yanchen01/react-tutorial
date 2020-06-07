fb85fef727f7c1da0b20fd60de0a766ef0559288
ychen1116 <ychen1116@gmail.com>
2020-06-06 23:09:16 -0400

    added component and state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, Children } from 'react';
 import './App.css';
 
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
+	state = {
+		persons: [
+			{
+				name: 'Max',
+				age: 28
+			},
+			{
+				name: 'Manu',
+				age: 29
+			},
+			{
+				name: 'Chen',
+				age: 19
+			}
+		],
+		otherState: 'some other value'
+	};
+
+	switchNameHandler = () => {
+		// console.log('Was Clicked!');
+		this.setState({
+			persons: [
+				{
+					name: 'Yan',
+					age: 28
+				},
+				{
+					name: 'Manu',
+					age: 29
+				},
+				{
+					name: 'Chen',
+					age: 20
+				}
+			]
+		});
+	};
+
 	render() {
 		return (
 			<div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+					My Hobbies: Racing
+				</Person>
+				<Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+				<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 			</div>
 		);
 	}
